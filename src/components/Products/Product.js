161c3654bfb66098345dff2ca6d978a8e8523ae1
shahariaz <shahariaz.info@gmail.com>
2023-08-29 17:00:50 +0600

    saving priveous code before starting todays code

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,24 +1,29 @@
+import React from "react";
 import "./Product.css";
-const Product = () => {
+
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>the lean Startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://m.media-amazon.com/images/I/71eDIz87SwL._AC_SL1500_.jpg"
-        alt=""
-      />
-      <button>Add to bucket</button>
+
+      <img src={image} alt="" />
+
+      <button>Add to Basket</button>
     </div>
   );
-};
+}
 
 export default Product;
